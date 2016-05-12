ba36081de3b4d453a802954cfc90bb47b6546c0a
Lorin VALTON <lorin.valton@gmail.com>
2016-05-12 10:23:58 +0200

    ajout du routage hello

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 root 'notes#index'  
 
+get 'hello', to: 'notes#hello'
+
 # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
